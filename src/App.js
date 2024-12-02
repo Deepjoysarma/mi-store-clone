62fab969e5eb9dc20a34e8daa33ec597e0712b79
Deepjoy Sarma <deepjoysarma1223@gmail.com>
2024-12-02 22:54:38 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import PreNavbar from './components/PreNavbar';
+import Navbar from "./components/Navbar.js"
+import {  BrowserRouter as Router,Route } from "react-router-dom"
+import Slider from "./components/Slider.js"
+import data from "./data/data.json"
+import Offers from './components/Offers.js';
+import Heading from "./components/Heading.js"
+import StarProduct from "./components/StarProduct.js"
+
 
 function App() {
   return (
-    <>
-      <h1>Welcome Mr. Deepjoy Sarma</h1>
-    </>
+    <Router>
+      <PreNavbar />
+      <Navbar />
+      <Slider start={data.banner.start} />
+      <Offers offer={data.offer} />
+      <Heading text="STAR PRODUCTS"/>
+      <StarProduct starProduct={data.starProduct} />
+    </Router>
   );
 }
 
