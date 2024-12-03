18774bf9923273bae8161d53875b747ebce9bf66
Deepjoy Sarma <deepjoysarma1223@gmail.com>
2024-12-03 11:57:31 +0530

    third

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import PreNavbar from './components/PreNavbar';
-import Navbar from "./components/Navbar.js"
-import {  BrowserRouter as Router,Route } from "react-router-dom"
-import Slider from "./components/Slider.js"
-import data from "./data/data.json"
+import Navbar from "./components/Navbar.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Slider from "./components/Slider.js";
+import data from "./data/data.json";
 import Offers from './components/Offers.js';
-import Heading from "./components/Heading.js"
-import StarProduct from "./components/StarProduct.js"
+import Heading from "./components/Heading.js";
+import StarProduct from "./components/StarProduct.js";
+import HotAccessoriesMenu from "./components/HotAccessoriesMenu.js";
+import HotAccessories from "./components/HotAccessories.js";
+import ProductReviews from "./components/ProductReviews.js";
+import Videos from "./components/Videos.js";
+import Banner from "./components/Banner.js";
+import Footer from "./components/Footer.js";
+
 
 
 function App() {
@@ -14,10 +21,30 @@ function App() {
     <Router>
       <PreNavbar />
       <Navbar />
+
       <Slider start={data.banner.start} />
       <Offers offer={data.offer} />
-      <Heading text="STAR PRODUCTS"/>
+      <Heading text="STAR PRODUCTS" />
       <StarProduct starProduct={data.starProduct} />
+      <Heading text="HOT ACCESSORIES" />
+      <HotAccessoriesMenu />
+
+      {/* Add Routes wrapper here */}
+      <Routes>
+        <Route path="/music" element={<HotAccessories music={data.hotAccessories.music} musicCover={data.hotAccessoriesCover.music} />} />
+        <Route path="/smartDevice" element={<HotAccessories smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice} />} />
+        <Route path="/home" element={<HotAccessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home} />} />
+        <Route path="/lifestyle" element={<HotAccessories lifeStyle={data.hotAccessories.lifeStyle} lifeStyleCover={data.hotAccessoriesCover.lifeStyle} />} />
+        <Route path="/mobileAccessories" element={<HotAccessories mobileAccessories={data.hotAccessories.mobileAccessories} mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories} />} />
+      </Routes>
+      
+      <Heading text="PRODUCT REVIEWS"/>
+      <ProductReviews productReviews={data.productReviews}/>
+      <Heading text="VIDEOS" />   
+      <Videos videos={data.videos} />
+      <Heading  text="IN THE PRESS"/>
+      <Banner  banner={data.banner}/>
+      <Footer footer={data.footer} />
     </Router>
   );
 }
